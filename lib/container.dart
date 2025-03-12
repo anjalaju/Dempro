284454173b21fcf74c19aa989d3cbb42e3b59d07
anjalaju <anjalkoranath@gmail.com>
2025-03-12 19:38:38 +0530

    12/8/2025

---
+++ lib/container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Containerr extends StatefulWidget {
+  const Containerr({super.key});
+
+  @override
+  State<Containerr> createState() => _ContainerrState();
+}
+
+class _ContainerrState extends State<Containerr> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
