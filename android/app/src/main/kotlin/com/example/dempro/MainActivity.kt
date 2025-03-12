0dea98b745dc1082421984921b6f3dbb7c0fdc7f
anjalaju <anjalkoranath@gmail.com>
2025-03-12 19:22:14 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/dempro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dempro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/homepage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("flex"),
+        // centerTitle: true,
+        backgroundColor: Colors.pink,
+        leading: const Icon(Icons.menu),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+        ],
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
+            // elevation: 4,shadowColor: Colors.black.withOpacity(0.5),
+      ),
+    );
+  }
+}
